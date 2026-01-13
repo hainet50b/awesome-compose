c1ddd057097dd6f7840afc08374fc44189449158
hainet50b <20400616+hainet50b@users.noreply.github.com>
2026-01-13 21:31:36 +0900

    Add simple Elasticsearch docker-compose and helper scripts

---
+++ elasticsearch-simple/docker-compose-logs-elasticsearch.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -eu
+
+script_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
+docker_compose_file='docker-compose.yml'
+service='pmacho-elasticsearch'
+
+docker compose \
+  --project-directory "$script_dir" \
+  -f "${script_dir}/${docker_compose_file}" \
+  logs -f "$service"
+
---
+++ elasticsearch-simple/elasticsearch-reset-password.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -eu
+
+script_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
+docker_compose_file='docker-compose.yml'
+service='pmacho-elasticsearch'
+
+docker compose \
+  --project-directory "$script_dir" \
+  -f "${script_dir}/${docker_compose_file}" \
+  exec "$service" \
+    /usr/share/elasticsearch/bin/elasticsearch-reset-password -u elastic -i
+
